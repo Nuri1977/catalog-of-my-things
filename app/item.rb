a99ffb5c49ba9bd7626dda50ef856035885e80dc
unknown <mugishasamuel42@gmail.com>
2022-04-18 03:24:08 -0700

    Add item class

---
+++ app/item.rb
@@ -0,0 +1,13 @@
+class Item
+  attr_accessor :name, :publish_date
+  attr_reader :id, :archived
+
+  # rubocop:disable Style/OptionalBooleanParameter
+  def initialize(name, publish_date, id = Random.rand(1..100_000), archived = false)
+    @id = id
+    @name = name
+    @publish_date = publish_date
+    @archived = archived
+  end
+  # rubocop:enable Style/OptionalBooleanParameter
+end
