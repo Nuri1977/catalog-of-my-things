d3e7ccf5e57df3e9ef7fc495fa4123c2563162d2
mugisha Samuel <mugishasamuel42@gmail.com>
2022-04-19 08:16:34 -0700

    Revert "Add unit tests"

--- app/label.rb
+++ app/label.rb
@@ -2,7 +2,7 @@ class Label
   attr_accessor :title, :color
   attr_reader :id, :items
 
-  def initialize(title, color, id = Random.rand(1..100_000))
+  def initialize(title, color, _items, id = Random.rand(1..100_000))
     @id = id
     @title = title
     @color = color
